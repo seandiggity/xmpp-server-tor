681d3e63478cb06be6624a232c1e1acec0a9c0e3
Sean O'Brien <sean@cyberclear.cc>
2023-12-19 07:13:59 -0500

    Updated install script for Debian Bookworm

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# This should be considered a fork of Micah Lee's tor-relay-bootstrap.
-# You can find that project here: https://github.com/micahflee/tor-relay-bootstrap
-# His bash script had exactly the structure I needed so it got repurposed.
+# "Paranoid Prosody" settings for config like OTR.im forked from https://github.com/NSAKEY/paranoid-prosody
+# Updated December 2023 for Debian Bookworm
+# Released under GNU General Public License v3 or any later version
 
-# check for root
+# Check for root
 if [[ $EUID -ne 0 ]]; then
     echo "This script must be run as root" 1>&2
     exit 1
@@ -12,18 +12,17 @@ fi
 
 PWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# update software
+# Update software
 echo "== Updating software"
 apt-get update
 apt-get dist-upgrade -y
 
-apt-get install -y lsb-release
-
+apt-get install -y
 
 # add official Tor repository
 if ! grep -q "http://deb.torproject.org/torproject.org" /etc/apt/sources.list; then
     echo "== Adding the official Tor repository"
-    echo "deb http://deb.torproject.org/torproject.org `lsb_release -cs` main" >> /etc/apt/sources.list
+    echo "deb http://deb.torproject.org/torproject.org bookworm main" >> /etc/apt/sources.list
     gpg --keyserver keys.gnupg.net --recv A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89
     gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | apt-key add -
     apt-get update
@@ -32,12 +31,12 @@ fi
 # Add the official Prosody repository
 if ! grep -q "deb http://packages.prosody.im/debian" /etc/apt/sources.list; then
     echo "== Adding the official Prosody repository"
-    echo "deb http://packages.prosody.im/debian `lsb_release -cs` main" >> /etc/apt/sources.list
+    echo "deb http://packages.prosody.im/debian bookworm main" >> /etc/apt/sources.list
     wget https://prosody.im/files/prosody-debian-packages.key -O- | sudo apt-key add -
     apt-get update
 fi
 
-# install prosody and tor, plus related packages
+# Install prosody and tor, plus related packages
 echo "== Installing Prosody and Tor"
 apt-get install -y deb.torproject.org-keyring tor tor-arm tor-geoipdb prosody lua-event
 service tor stop
@@ -50,13 +49,13 @@ openssl dhparam -out /etc/prosody/certs/dhparam.pem 4096
 chown prosody:prosody /etc/prosody/certs/*
 chmod 600 /etc/prosody/certs/*
 
-# configure tor
+# Configure tor
 cp $PWD/etc/tor/torrc /etc/tor/torrc
 
-# configure prosody
+# Configure prosody
 cp $PWD/etc/prosody/prosody.cfg.lua /etc/prosody/prosody.cfg.lua
 
-# configure firewall rules
+# Configure firewall rules
 echo "== Configuring firewall rules"
 apt-get install -y debconf-utils
 echo "iptables-persistent iptables-persistent/autosave_v6 boolean true" | debconf-set-selections
@@ -69,35 +68,22 @@ chmod 600 /etc/iptables/rules.v6
 iptables-restore < /etc/iptables/rules.v4
 ip6tables-restore < /etc/iptables/rules.v6
 
-# configure automatic updates
+# Configure automatic updates
 echo "== Configuring unattended upgrades"
 apt-get install -y unattended-upgrades apt-listchanges
 cp $PWD/etc/apt/apt.conf.d/20auto-upgrades /etc/apt/apt.conf.d/20auto-upgrades
 service unattended-upgrades restart
 
-# install and configure apparmor
+# Install and configure apparmor
 apt-get install -y apparmor apparmor-profiles apparmor-utils
 sed -i.bak 's/GRUB_CMDLINE_LINUX="\(.*\)"/GRUB_CMDLINE_LINUX="\1 apparmor=1 security=apparmor"/' /etc/default/grub
 update-grub
 cp $PWD/etc/apparmor.d/usr.bin.prosody /etc/apparmor.d/usr.bin.prosody
 
-# install tlsdate
-if [ "$(lsb_release -cs)" == "wheezy" ]; then
-	# tlsdate isn't in wheezy
-	if [ "$((echo 3.5; uname -r) | sort -cV 2>&1)" == "" ]; then
-		# if we have seccomp (>= linux 3.5) we can backport it
-		if ! grep -q "wheezy-backports" /etc/apt/sources.list; then
-			echo "deb http://ftp.debian.org/debian wheezy-backports main" >> /etc/apt/sources.list
-			apt-get update
-		fi
-		apt-get install -y tlsdate
-	fi
-else
-	# later than wheezy
-	apt-get install -y tlsdate
-fi
+# Install tlsdate
+apt-get install -y tlsdate
 
-# final instructions
+# Final instructions
 echo ""
 echo "== Try SSHing into this server again in a new window as well as connecting to XMPP, to confirm the firewall isn't broken"
 echo ""
